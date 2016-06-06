449161890a7d87a2e222b78ff39208c24941c794
Dominick Baier <dbaier@leastprivilege.com>
2016-06-06 14:14:06 +0200

    add idsrv start

---
+++ IdentityServerStart/src/IdentityServerStart/Configuration/Clients.cs
@@ -0,0 +1,16 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Host.Configuration
+{
+    public class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+            {
+                
+            };
+        }
+    }
+}
---
+++ IdentityServerStart/src/IdentityServerStart/Configuration/Scopes.cs
@@ -0,0 +1,16 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Host.Configuration
+{
+    public class Scopes
+    {
+        public static IEnumerable<Scope> Get()
+        {
+            return new List<Scope>
+            {
+                
+            };
+        }
+    }
+}
