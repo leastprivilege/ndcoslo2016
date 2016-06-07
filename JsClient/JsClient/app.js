167ef2673d75fdf300d770d7be5f2fab9a6bb3fd
Brock Allen <brockallen@gmail.com>
2016-06-07 14:49:56 +0200

    start to js client

---
+++ JsClient/JsClient/app.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="oidc-client.js" />
+
+document.getElementById("login").addEventListener("click", doLogin, false);
+document.getElementById("callApi").addEventListener("click", callApi, false);
+
+
+function doLogin() {
+
+}
+
+
+function callApi() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:48791/test");
+    xhr.onload = function () {
+        var data = JSON.parse(xhr.responseText);
+        log("API Result", data);
+    }
+    xhr.send();
+}
+
+
+function log() {
+    document.getElementById('out').innerText = '';
+
+    Array.prototype.forEach.call(arguments, function (msg) {
+        if (msg instanceof Error) {
+            msg = "Error: " + msg.message;
+        }
+        else if (typeof msg !== 'string') {
+            msg = JSON.stringify(msg, null, 2);
+        }
+        document.getElementById('out').innerHTML += msg + '\r\n';
+    });
+}
+
